dc1aeafa310c89b91d36028a816fc89d21d7ddaa
JohanitaM <jole360@hotmail.es>
2023-07-24 13:49:45 -0500

    feat: cambio en en tamaño del texto de las publicaciones

--- observatorio/src/App.jsx
+++ observatorio/src/App.jsx
@@ -1,34 +1,13 @@
 import './App.scss';
 import Obs from './componentes/Obs/Obs';
-import Formulario from './Formulario/componentes/Form/Formulario';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 
 function App() {
 
   return (
-    <Router>
     <div>
-      <nav>
-        <ul>
-          <li style={{ listStyle: 'none' }}>
-            Obs
-            <Link to="/"></Link>
-          </li>
-          <li style={{ listStyle: 'none' }}>
-            Formulario
-            <Link to="/formulario"></Link>
-          </li>
-        </ul>
-      </nav>
-
-    
-      <Routes>
-        <Route path="/" element={<Obs />} />
-        <Route path="/formulario" element={<Formulario />} />
-      </Routes>
+      <Obs />
     </div>
-  </Router>
 );
 }
 
